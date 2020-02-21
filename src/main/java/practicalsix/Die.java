62d0cd96aa8c13aa87c6da9554c6872c7dae4fdc
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-02-21 07:24:27 -0500

    Add the starting version of the Die class with the TODO markers.

---
+++ src/main/java/practicalsix/Die.java
@@ -0,0 +1,33 @@
+// TODO: Add the correct package declaration
+
+// TODO: Add all of the required JavaDoc comments to this class
+
+// TODO: Make sure that you refer to the textbook for source code
+
+public class Die {
+
+  private static final int MAX = 6;
+  private int faceValue;
+
+  public Die() {
+    faceValue = 1;
+  }
+
+  public int roll() {
+    // TODO: Add the implementation of this method
+  }
+
+  public void setFaceValue(int value) {
+    // TODO: Add the implementation of this method
+  }
+
+  public int getFaceValue() {
+    // TODO: Add the implementation of this method
+  }
+
+  public String toString() {
+    String result = Integer.toString(faceValue);
+    return result;
+  }
+
+}
